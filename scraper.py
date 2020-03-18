a503d78520295eef4a356a7b92df4a4905f02270
pwazydrag <pwazydrag@gmail.com>
2020-03-18 15:24:40 +0100

    2020-03-18

--- scraper.py
+++ scraper.py
@@ -15,19 +15,35 @@ opinions = page_tree.find_all("li", "review-box")
 #wydobycie składowych dla pojedynczych opinii
 opinion = opinions.pop()
 
-opinion_id = opinion["data-entry-id"]
+for opinion in opinions:
+    opinion_id = opinion["data-entry-id"]
+    author = opinion.find("div", "reviewer-name-line").string
+    try:
+        recommendation = opinion.find("div", "product-review-summary").find("em").string
+    except AttributeError:
+        recommendation = None
+    stars = opinion.find("span", "review-score-count").string
+    try:
+        purchased = opinion.find("div", "product-review-pz").string
+    except AttributeError:
+        purchased = None
+    dates = opinion.find("span", "review-time").find_all("time")
+    review_date = dates.pop(0)["datetime"]
+    try:
+        purchase_date = dates.pop(0)["datetime"]
+    except IndexError:
+        purchase_date = None
+    useful = opinion.find("button", "vote-yes").find("span").string
 
-author = opinion.find("div", "reviewer-name-line").string
+    useless = opinion.find("button", "vote-no").find("span").string
 
-recommendation = opinion.find("div", "product-review-summary").find("em").string
-
-stars = opinion.find("span", "review-score-count").string
-
-purchased = opinion.find("div", "product-review-pz").string
-
-useful = opinion.find("button", "vote-yes").find("span").string
-
-useless = opinion.find("button", "vote-no").find("span").string
-
-content=opinion.find("p","product-review-body").get_text()
-
+    content=opinion.find("p","product-review-body").get_text()
+    try:
+        pros = opinion.find("div", "pros-cell").find("ul").get_text()
+    except AttributeError:
+        pros = None
+    try:
+        cons = opinion.find("div", "cons-cell").find("ul").get_text()
+    except AttributeError:
+        cons = None
+print(author)
